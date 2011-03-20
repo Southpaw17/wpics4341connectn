7cbe8c502a429b4f0c6c46bb78fbf670a100a947
Roj.Nemennuh.Sarton <Roj.Nemennuh.Sarton@e8f45863-ed75-c26e-d2eb-d2fe4336f9a3>
2011-03-20 23:21:30 +0000

    project import

---
+++ BOrMuezaersas_Agent_Connect_N/src/edu/wpi/cs4341/connectn/ConnectNAgent.java
@@ -0,0 +1,13 @@
+package edu.wpi.cs4341.connectn;
+
+public class ConnectNAgent {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ BOrMuezaersas_Agent_Connect_N/src/edu/wpi/cs4341/connectn/MoveCalculator.java
@@ -0,0 +1,11 @@
+package edu.wpi.cs4341.connectn;
+
+public class MoveCalculator implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
