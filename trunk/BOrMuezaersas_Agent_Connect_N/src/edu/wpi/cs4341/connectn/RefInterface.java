d356bad11b724fc229393665e324f73bad40c4e2
Roj.Nemennuh.Sarton <Roj.Nemennuh.Sarton@e8f45863-ed75-c26e-d2eb-d2fe4336f9a3>
2011-03-20 23:21:30 +0000

    project import

---
+++ trunk/BOrMuezaersas_Agent_Connect_N/src/edu/wpi/cs4341/connectn/RefInterface.java
@@ -0,0 +1,13 @@
+package edu.wpi.cs4341.connectn;
+
+import java.util.TimerTask;
+
+public class RefInterface extends TimerTask {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
