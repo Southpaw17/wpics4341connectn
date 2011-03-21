42fc3e92e35b4b161151b1d1a495c411f4991fac
bruzzese.frank@gmail.com <bruzzese.frank@gmail.com@e8f45863-ed75-c26e-d2eb-d2fe4336f9a3>
2011-03-21 23:50:40 +0000

    Added a state class and began defining it

--- trunk/BOrMuezaersas_Agent_Connect_N/src/edu/wpi/cs4341/connectn/MoveCalculator.java
+++ trunk/BOrMuezaersas_Agent_Connect_N/src/edu/wpi/cs4341/connectn/MoveCalculator.java
@@ -8,4 +8,58 @@ public class MoveCalculator implements Runnable{
 		
 	}
 
+	// TODO NEED PUBLIC REGISTER_MOVE FUNCTION
+	// TODO NEED GetCurrentBestMove() FUNCTION
+}
+
+// This class stores the state of a game 
+private class State {
+	
+	private int[][] state;		// stores the state
+	private State[] children;	// stores the children of this state
+	private int heuristic;		// stores the heuristic value of this state
+	
+	public State( ) {
+		
+	}
+	
+	/**
+	 * Initializes the State class with a predefined state
+	 * 
+	 * @param newstate - the state you want this state to store and evaluate
+	 */
+	public State( int[][] newstate ) {
+		state = newstate;
+		
+		CalculateHeuristic();
+	}
+	
+	private void CalculateHeuristic(){
+		// TODO add functionality to base heuristic on children
+		
+		heuristic = 5;
+	}
+	
+	/**
+	 * Returns the heuristic value for this state
+	 * 
+	 * @return returns the heuristic
+	 */
+	public int GetHeuristic() {
+		return heuristic;
+	}
+	
+	/**
+	 * Creates the children for this state.  These are assigned
+	 * to an array since there will always be the same number of
+	 * children (equal to the branching factor) which can be sent
+	 * to the move calculator to determine which should be persued
+	 * further.
+	 */
+	public void MakeBabies() {
+		// TODO create children and store them in the array
+		
+		CalculateHeuristic();	// recalculates the heuristic for this state based on the heuristic values of its children
+	}
+	
 }
