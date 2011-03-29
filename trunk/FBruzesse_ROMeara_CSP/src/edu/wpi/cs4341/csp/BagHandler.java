9a95485ca569c25fd6d67027a0f7adf793da9fd3
Roj.Nemennuh.Sarton <Roj.Nemennuh.Sarton@e8f45863-ed75-c26e-d2eb-d2fe4336f9a3>
2011-03-29 23:00:15 +0000

    Added basic methods to BagHandler

--- trunk/FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/BagHandler.java
+++ trunk/FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/BagHandler.java
@@ -1,5 +1,13 @@
 package edu.wpi.cs4341.csp;
 
 public class BagHandler {
-
+	
+	public Bag getBag(String bagLabel){
+		//TODO implement
+		return null;
+	}
+	
+	public Bag[] getAllBags(){
+		return null;
+	}
 }
