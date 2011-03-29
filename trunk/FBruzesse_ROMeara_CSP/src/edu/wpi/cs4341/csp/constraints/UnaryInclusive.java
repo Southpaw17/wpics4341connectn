f365ea450d516d38fa6ea2b4ff5e5850f80cf5ec
Roj.Nemennuh.Sarton <Roj.Nemennuh.Sarton@e8f45863-ed75-c26e-d2eb-d2fe4336f9a3>
2011-03-29 23:18:57 +0000

    Changed method to be useful

--- trunk/FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/constraints/UnaryInclusive.java
+++ trunk/FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/constraints/UnaryInclusive.java
@@ -1,6 +1,5 @@
 package edu.wpi.cs4341.csp.constraints;
 
-import java.util.ArrayList;
 
 import edu.wpi.cs4341.csp.Bag;
 import edu.wpi.cs4341.csp.BagHandler;
@@ -8,7 +7,7 @@ import edu.wpi.cs4341.csp.BagHandler;
 public class UnaryInclusive implements Constraint {
 
 	@Override
-	public ArrayList<Bag> apply(BagHandler currentHandler) {
+	public Bag[] apply(BagHandler currentHandler, Bag[] currentBags) {
 		// TODO Auto-generated method stub
 		return null;
 	}
