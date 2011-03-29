c95cb4507db8637d7b1d5db59a32da5b9259544e
bruzzese.frank@gmail.com <bruzzese.frank@gmail.com@e8f45863-ed75-c26e-d2eb-d2fe4336f9a3>
2011-03-29 23:02:12 +0000

    ABOUT FUCKING TIME I WROTE SOMETHING

--- FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/Item.java
+++ FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/Item.java
@@ -1,5 +1,18 @@
 package edu.wpi.cs4341.csp;
 
+import java.util.ArrayList;
+
+import edu.wpi.cs4341.csp.constraints.Constraint;
+
 public class Item {
 
+	int weight;
+	ArrayList<Constraint> constraints;
+	
+	public Item( int w ) {
+		
+	}
+	
+	
+	
 }
