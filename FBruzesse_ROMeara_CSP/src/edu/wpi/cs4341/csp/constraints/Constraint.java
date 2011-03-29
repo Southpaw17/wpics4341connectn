d98eb85743c98cd16663dc27c00de341ced2cb69
Roj.Nemennuh.Sarton <Roj.Nemennuh.Sarton@e8f45863-ed75-c26e-d2eb-d2fe4336f9a3>
2011-03-29 23:18:57 +0000

    Changed method to be useful

--- FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/constraints/Constraint.java
+++ FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/constraints/Constraint.java
@@ -1,6 +1,5 @@
 package edu.wpi.cs4341.csp.constraints;
 
-import java.util.ArrayList;
 
 import edu.wpi.cs4341.csp.Bag;
 import edu.wpi.cs4341.csp.BagHandler;
@@ -15,7 +14,8 @@ public interface Constraint {
 	/**
 	 * Applies this constraint to the set of bags using in given bag handler
 	 * @param currentHandler The current bag handler (and set of bags)
+	 * @param currentBags TODO
 	 * @return The allowable set of bags
 	 */
-	public abstract ArrayList<Bag> apply(BagHandler currentHandler);
+	public abstract Bag[] apply(BagHandler currentHandler, Bag[] currentBags);
 }
