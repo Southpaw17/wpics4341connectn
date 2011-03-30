8f40929925d53b0a0dabeab4028442967b4ae505
Roj.Nemennuh.Sarton <Roj.Nemennuh.Sarton@e8f45863-ed75-c26e-d2eb-d2fe4336f9a3>
2011-03-30 15:40:50 +0000

    Added commented layout

--- trunk/FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/CSPMain.java
+++ trunk/FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/CSPMain.java
@@ -2,6 +2,22 @@ package edu.wpi.cs4341.csp;
 
 public class CSPMain {
 	public static void main(String[] args){
+		//create starting set
 		
+		//loop through all possible sets
+			//check if complete set
+				//if not, advance to next set
+					//update sets of items not placed to reflect this set
+					//choose next item to place
+					//place item
+					//check if complete set, if yes, mark bag handler as complete
+		
+		
+		//compare and choose
+			//first, elimiate any which have both less weight and items
+			//next, choose based on % wasted space
+			//if still tie, pick based on evenness of wieght distribution
+			//if STILL tie, pick based on evenness of item distribution
+			//if STILL tie, just pick one (honestly, they are essentially exactly equivalent at this point)
 	}
 }
