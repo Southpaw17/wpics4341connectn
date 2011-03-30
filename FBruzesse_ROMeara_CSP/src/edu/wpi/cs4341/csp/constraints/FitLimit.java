d7dd7ab34836ed39972328457b2a9c4fd743ba2b
Roj.Nemennuh.Sarton <Roj.Nemennuh.Sarton@e8f45863-ed75-c26e-d2eb-d2fe4336f9a3>
2011-03-30 01:11:15 +0000

    Finished first writing of parsing functions

--- FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/constraints/FitLimit.java
+++ FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/constraints/FitLimit.java
@@ -7,11 +7,24 @@ import edu.wpi.cs4341.csp.Bag;
 import edu.wpi.cs4341.csp.BagHandler;
 
 public class FitLimit implements Constraint {
-
+	int lower, higher;
+	
 	@Override
 	public ArrayList<Bag> apply(BagHandler currentHandler, Bag[] currentBags) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Bag> temp = new ArrayList<Bag>();
+		
+		for(Bag b : currentBags){
+			if((b.getNumberItems() >= lower)&&(b.getNumberItems() <= higher)){
+				temp.add(b);
+			}
+		}
+		
+		return temp;
+	}
+	
+	public FitLimit(int low, int high){
+		lower = low;
+		higher = high;
 	}
 
 }
