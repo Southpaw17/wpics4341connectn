c9cc9291eca31a3d31807c8b635ad0e580033eda
Roj.Nemennuh.Sarton <Roj.Nemennuh.Sarton@e8f45863-ed75-c26e-d2eb-d2fe4336f9a3>
2011-03-30 23:26:12 +0000

    Added stat functions

--- trunk/FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/BagHandler.java
+++ trunk/FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/BagHandler.java
@@ -5,14 +5,103 @@ import java.util.ArrayList;
 import edu.wpi.cs4341.csp.constraints.Constraint;
 
 public class BagHandler {
+	protected boolean complete;
 	ArrayList<Bag> bags;					//list of bags
 	ArrayList<Constraint> validChecks;		//List of checks.  Each one will return list of bags which do  
-											//satisify the constraint.  Imdividual constraints will include in
+											//satisfy the constraint.  Individual constraints will include in
 											//constructor which bag to check
 	
 	public BagHandler(){
+		complete = false;
 		bags = new ArrayList<Bag>();
 		validChecks = new ArrayList<Constraint>();
+	}
+	
+	/**
+	 * Checks if this set is complete
+	 * @param items The items being put in bags
+	 */
+	public void checkComplete(Item[] items){
+		boolean found = false;
+		
+		for(Item i : items){
+			found = false;
+			
+			for(Bag b : bags){
+				if(b.containsItem(i)){
+					found = true;
+					break;
+				}
+			}
+			
+			if(!found){
+				//if still possible sets, not complete, exit
+				if(i.applyConstraints(this).size() > 0){
+					complete = false;
+					return;
+				}
+			}
+		}
+		
+		complete = true;
+	}
+	
+	/** @return true if this is a complete set, false otherwise */
+	public boolean isComplete(){return complete;}
+	
+	/** @return The number of items in bags in this set */
+	public int totalItems(){
+		int ret = 0;
+		
+		for(Bag b : bags){ret += b.getNumberItems();}
+		
+		return ret;
+	}
+	
+	/** @return The total weight of the bags in this set */
+	public int totalWeight(){
+		int ret = 0;
+		
+		for(Bag b : bags){ret += (b.getPossibleCapacity() - b.getRemainingCapacity());}
+		
+		return ret;
+	}
+	
+	/** @return The units of unfilled weight in this set */
+	public int wastedWeight(){
+		int ret = 0;
+		
+		for(Bag b : bags){ret += b.getRemainingCapacity();}
+		
+		return ret;
+	}
+	
+	/** @return The standard deviation of weight in bags in this set */
+	public int weightEvenness(){
+		int mean = totalWeight()/bags.size();
+		int meanDev = 0;
+		
+		for(Bag b : bags){
+			meanDev += Math.abs(mean - (b.getPossibleCapacity() - b.getRemainingCapacity()));
+		}
+		
+		meanDev /= bags.size();
+		
+		return meanDev;
+	}
+	
+	/** @return The standard deviation of items in bags in this set */
+	public int itemEvenness(){
+		int mean = totalItems()/bags.size();
+		int meanDev = 0;
+		
+		for(Bag b : bags){
+			meanDev += Math.abs(mean - b.getNumberItems());
+		}
+		
+		meanDev /= bags.size();
+		
+		return meanDev;
 	}
 	
 	/**
