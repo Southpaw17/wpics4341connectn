1be30b8ceb12af17b10c0c0b73ab43786e54608b
Roj.Nemennuh.Sarton <Roj.Nemennuh.Sarton@e8f45863-ed75-c26e-d2eb-d2fe4336f9a3>
2011-03-31 01:21:54 +0000

    Added is- functions and implement getchildren

--- trunk/FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/SetHandler.java
+++ trunk/FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/SetHandler.java
@@ -26,6 +26,59 @@ public class SetHandler {
 	}
 	
 	public SetHandler[] getChildren(){
-		return null;
+		if((itemSet == null)||(itemSet.size() == 0)){return null;}
+		
+		Item toPlace = itemSet.get(0);
+		ArrayList<Bag> possibleBags = itemSet.get(0).applyConstraints(bagSet);
+		
+		//find item to place
+		for(int i = 1; i < itemSet.size(); i++){
+			ArrayList<Bag> temp = itemSet.get(i).applyConstraints(bagSet);
+			if((temp.size() != 0)&&(temp.size() < possibleBags.size())){
+				toPlace = itemSet.get(i);
+				possibleBags = temp;
+			}else if(temp.size() == possibleBags.size()){
+				if(itemSet.get(i).getNumConstraints() < toPlace.getNumConstraints()){
+					toPlace = itemSet.get(i);
+					possibleBags = temp;
+				}
+			}else if((temp.size() != 0)&&(possibleBags.size() == 0)){
+				toPlace = itemSet.get(i);
+				possibleBags = temp;
+			}
+		}
+		
+		ArrayList<Item> newSet = new ArrayList<Item>();
+		
+		for(Item i : itemSet){
+			if(!i.getLabel().equals(toPlace.getLabel())){
+				newSet.add(i);
+			}
+		}
+		
+		ArrayList<SetHandler> sets = new ArrayList<SetHandler>();
+		
+		for(Bag b : possibleBags){
+			BagHandler newHand = bagSet.copyHandler();
+			newHand.getBag(b.getBagName()).addToBag(toPlace);
+			sets.add(new SetHandler(newSet.toArray(new Item[newSet.size()]), newHand));
+		}
+		
+		return sets.toArray(new SetHandler[sets.size()]);
+	}
+	
+	public boolean isValid(){
+		boolean isValid = bagSet.isValidSolution();
+		
+		for(Item i : itemSet){
+			if(i.applyConstraints(bagSet).size() == 0){isValid = false;}
+		}
+		
+		return isValid;
+	}
+	
+	public boolean isComplete(){
+		bagSet.checkComplete(itemSet.toArray(new Item[itemSet.size()]));
+		return bagSet.isComplete();
 	}
 }
