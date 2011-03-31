cb81198fc370934b9305c1e335886f4cbf0955bd
Roj.Nemennuh.Sarton <Roj.Nemennuh.Sarton@e8f45863-ed75-c26e-d2eb-d2fe4336f9a3>
2011-03-31 10:34:29 +0000

    Stupid broken version

--- trunk/FBruzesse_ROMeara_CSP/src/edu/wpi/cs4341/csp/heuristic/ItemHeuristic.java
+++
@@ -1,8 +0,0 @@
-package edu.wpi.cs4341.csp.heuristic;
-
-import edu.wpi.cs4341.csp.Bag;
-import edu.wpi.cs4341.csp.Item;
-
-public interface ItemHeuristic {
-	public abstract Item selectItem(Item[] itemSet, Bag[] bagSet);
-}
